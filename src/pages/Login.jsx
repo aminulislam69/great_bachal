7d33166c59f0af7c24809b760ae39e404d004aed
aminulislam69 <119211802+aminulislam69@users.noreply.github.com>
2023-07-24 22:39:51 +0600

    goole sign up

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,28 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Grid, TextField, Button} from '@mui/material';
 import Headingforreglog from '../assets/components/Headingforreglog';
 import Loginpic from '../assets/Loginpic.png'
 import Google from '../assets/Google.png'
+import LoadingButton from '@mui/lab/LoadingButton';
+import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider} from "firebase/auth";
+
+
+
+let innitialValue = {
+  email:"",
+  password:"",
+  loding: false
+}
 
 const Login = () => {
+
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+
+  let [values, setValues] = useState(innitialValue)
+
+  
+let handlevalue = (e) => {
+  setValues({
+    ...values,
+    [e.target.name]: e.target.value
+  })
+  
+}
+
+let handleclick = () =>{
+  let { email, password} = values
+
+  setValues({
+    ...values,
+    loding: true
+  })
+
+  signInWithEmailAndPassword(auth, email, password).then((user)=>{
+    
+    setValues({
+      email:"",
+      password:"",
+      loding: false
+    })
+    // navigate('/login')
+    console.log(user)
+  })
+}
+
+
+let handleGoogleLogin = () =>{
+  signInWithPopup(auth, provider).then((result)=>{
+    console.log(result)
+  })
+}
+
+
   return (
    <>
     <Grid container spacing={2}>
         <Grid item xs={6}>
           <div className='regcontainer'>
             <Headingforreglog className="headingeglog" titel="Login to your account!"/>
-            <img className='googleimg' src={Google}/>
+            <img onClick={handleGoogleLogin} className='googleimg' src={Google}/>
           
             <div className='reginput'> 
-                <TextField id="outlined-basic" label="Email Address" variant="outlined" />
+                <TextField value={values.email} onChange={handlevalue} name='email' id="outlined-basic" label="Email Address" variant="outlined" />
             </div>
          
             <div className='reginput'>
-                <TextField id="outlined-basic" label="Password" variant="outlined" />
+                <TextField value={values.password}  onChange={handlevalue} name='password' id="outlined-basic" label="Password" variant="outlined" />
             </div>
-            <div className='regbttn'>
-                <Button variant="contained">Login to Continue</Button>
-            </div>
+            {values.loding ?
+              <div className='regbttn'>
+                    <LoadingButton loading variant="outlined">
+                      Submit
+                    </LoadingButton> 
+              </div>
+                :
+              <div className='regbttn'>
+                    <Button onClick={handleclick} variant="contained">Login to Continue</Button>
+              </div>
+           }
+            
             </div>
             
         </Grid>
--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Grid, TextField, Button} from '@mui/material';
 import Resistrationimg from '../assets/Resistration.png'
 import Headingforreglog from '../assets/components/Headingforreglog';
+import { getAuth, createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
+import LoadingButton from '@mui/lab/LoadingButton';
+import { useNavigate } from 'react-router-dom';
+
+
+let innitialValue = {
+  email:"",
+  fullname:"",
+  password:"",
+  loding: false
+
+}
 
 const Registration = () => {
+
+  const auth = getAuth();
+
+  let navigate = useNavigate()
+
+  let [values, setValues] = useState(innitialValue)
+
+let handlevalue = (e) => {
+  setValues({
+    ...values,
+    [e.target.name]: e.target.value
+  })
+  
+}
+
+
+let handleclick = () =>{
+  let { email, fullname, password} = values
+
+  setValues({
+    ...values,
+    loding: true
+  })
+
+  createUserWithEmailAndPassword(auth, email, password).then((user)=>{
+    const auth = getAuth();
+sendEmailVerification(auth.currentUser)
+  .then(() => {
+    console.log("email sent")
+  });
+    setValues({
+      email:"",
+      fullname:"",
+      password:"",
+      loding: false
+    })
+    // navigate('/login')
+  })
+}
+
+
   return (
    <>
     <Grid container spacing={2}>
@@ -12,17 +65,27 @@ const Registration = () => {
             <Headingforreglog className="headingeglog" titel="Get started with easily register"/>
             <p>Free register and you can enjoy it</p>
             <div className='reginput'> 
-                <TextField id="outlined-basic" label="Email Address" variant="outlined" />
+                <TextField value={values.email} onChange={handlevalue} name='email' id="outlined-basic" label="Email Address" variant="outlined" />
             </div>
             <div className='reginput'>
-                <TextField id="outlined-basic" label="Ful name" variant="outlined" /> 
+                <TextField value={values.fullname}  onChange={handlevalue} name='fullname' id="outlined-basic" label="Full name" variant="outlined" /> 
             </div>
             <div className='reginput'>
-                <TextField id="outlined-basic" label="Password" variant="outlined" />
+                <TextField value={values.password}  onChange={handlevalue} name='password' id="outlined-basic" label="Password" variant="outlined" />
             </div>
-            <div className='regbttn'>
-                <Button variant="contained">Sign up</Button>
-            </div>
+
+            {values.loding ?
+              <div className='regbttn'>
+                    <LoadingButton loading variant="outlined">
+                      Submit
+                    </LoadingButton> 
+              </div>
+                :
+              <div className='regbttn'>
+                    <Button onClick={handleclick} variant="contained">Sign up</Button>
+              </div>
+           }
+            
             </div>
             
         </Grid>
